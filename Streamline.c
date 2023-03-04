a2f7c011f4dbd92637ddd6c06108f56c54da466a
Shrikrishna-Bhat <118532304+Shrikrishna-Bhat@users.noreply.github.com>
2023-03-04 18:19:42 +0530

    Update Streamline.c

--- Streamline.c
+++ Streamline.c
@@ -6,16 +6,21 @@
 
 int main()
 {
-  int randomNumber, randomNumberArray[15];
+  int randomNumber, randomNumberArray[50];
     
   for (int i=0; i<50; i++)
   {
       randomNumberArray[i] = (rand() % 100) + 1;
   }
 
-  for (int i=0; i<15; i++)
+  for (int i=0; i<50; i++)
   {
       printf("Random numbers %d \n",randomNumberArray[i]);
   }
+  return 0;
+}
+
+int getTemparetureSensorData()
+{
   return 0;
 }
